496cbf8510fccf8ac0e7df138eeb66da6cb3cc37
clippedtoperfection-boop <clippedtoperfection@gmail.com>
2026-05-29 13:56:24 -0400

    add app/layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,5 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css'
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (<ClerkProvider><html lang="en"><body>{children}</body></html></ClerkProvider>)
+}
