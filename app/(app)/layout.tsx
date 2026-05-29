c0a87b75aa8da436cbd591b122ed4d08c4e24776
clippedtoperfection-boop <clippedtoperfection@gmail.com>
2026-05-29 16:57:34 -0400

    fix all pages

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import AppLayout from '@/app/_app_layout'
 import { ReactNode } from 'react'
-export default function Layout({ children }: { children: ReactNode }) {
+
+export default function Layout({children}:{children:ReactNode}) {
   return <AppLayout>{children}</AppLayout>
 }
