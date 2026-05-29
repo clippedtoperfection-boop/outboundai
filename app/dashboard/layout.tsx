88445e84413c2f05186b07c6297165e87a7a84a0
clippedtoperfection-boop <clippedtoperfection@gmail.com>
2026-05-29 17:29:58 -0400

    add layout to dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,5 @@
+import AppLayout from '@/app/_app_layout'
+import { ReactNode } from 'react'
+export default function Layout({ children }: { children: ReactNode }) {
+  return <AppLayout>{children}</AppLayout>
+}
