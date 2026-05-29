6c148832574e5c23d2e8f7a82ce5bece3d777098
clippedtoperfection-boop <clippedtoperfection@gmail.com>
2026-05-29 16:57:13 -0400

    fix sign-in/sign-up syntax

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
+
 export default function Page() {
-  return <div style={{display:'flex',height:'100vh',alignItems:'center''justifyContent:'center',background:'var(--bg)'}}><SignUp/></div>
+  return (
+    <div style={{display:'flex',height:'100vh',alignItems:'center',justifyContent:'center',background:'var(--bg)'}}>
+      <SignUp />
+    </div>
+  )
 }
