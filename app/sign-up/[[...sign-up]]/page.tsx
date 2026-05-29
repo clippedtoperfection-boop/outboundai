27933db64234c3b910e64b6612ada4eeb2d10b3b
clippedtoperfection-boop <clippedtoperfection@gmail.com>
2026-05-29 14:14:22 -0400

    add app/sign-up/[[...sign-up]]/page.tsx

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,4 @@
+import { SignUp } from '@clerk/nextjs'
+export default function Page() {
+  return <div style={{display:'flex',height:'100vh',alignItems:'center''justifyContent:'center',background:'var(--bg)'}}><SignUp/></div>
+}
